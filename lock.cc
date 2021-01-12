f895f114e698c5184e53b66daad06fe470dc80cd
yuecheng.pei <793499453@qq.com>
2021-01-12 23:53:41 +0800

    Add more examples

--- lock.cc
+++ lock.cc
@@ -13,25 +13,30 @@
                   << delta##label.count()  << "s" << std::endl;
 
 static omp_lock_t lock;
-void putMes(int i)
-{
+void lock_example(int i) {
     std::cout << i << ":AA" << std::endl;
     omp_set_lock(&lock); //获得互斥器
     // sleep(1);
-    std::cout << i << ":BB1" << std::endl;
+    std::cout << i << ":BB" << std::endl;
     // sleep(1);
-    std::cout << i << ":BB2" << std::endl;
+    std::cout << i << ":BB" << std::endl;
     omp_unset_lock(&lock); //释放互斥器
 }
 
-int main()
-{
+int main() {
     omp_init_lock(&lock); // 初始化互斥锁
-#pragma omp parallel for
-    for (int i = 0; i < 4; ++i)
-    {
-        putMes(omp_get_thread_num());
+	#pragma omp parallel for
+    for (int i = 0; i < 4; ++i) {
+        lock_example(omp_get_thread_num());
     }
     omp_destroy_lock(&lock); //销毁互斥器
-    return 0;
+	int sum = 0;
+	#pragma omp parallel for
+    for (int i = 0; i < 10000; ++i) {
+		#pragma omp critical  // 临界区
+		{
+          sum = sum + i;
+        }
+    }	
+	std::cout << "sum: " << sum << std::endl;
 }
---
+++ thread_assign_tasks.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <omp.h>
+#include <chrono>
+#include <thread>
+
+using namespace std;
+
+int main() {
+	#pragma omp parallel num_threads(4)
+	{
+    	#pragma omp master  // 有且只有主线程能(0号线程)执行 {} 中的代码
+    	{
+      		cout << "master thread_id: " << omp_get_thread_num() << endl;
+        }
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    	cout << "sub threads is running" << endl; // 子线程执行此处代码
+	}
+
+	#pragma omp parallel sections
+	{
+    	#pragma omp section // 一个 section 只会被一个线程执行一次， section 之间是并行的
+    	{
+			std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        	cout << "section 1,threadid=" << omp_get_thread_num() << endl;
+    	}
+  
+	    #pragma omp section
+    	{
+			std::this_thread::sleep_for(std::chrono::milliseconds(1500));
+        	cout << "section 2,threadid=" << omp_get_thread_num() << endl;
+    	}
+   		#pragma omp section
+    	{
+        	cout << "section 3,threadid=" << omp_get_thread_num() << endl;
+    	}
+	}
+	int a[3]{0, 1, 2};
+	auto func = [](int i) {	
+			std::this_thread::sleep_for(std::chrono::milliseconds(100*i));
+			cout << "task_id: " << i << " thread_id: " << omp_get_thread_num() << endl; 
+		};
+	
+	#pragma omp parallel
+	{
+    	#pragma omp single  // 表示只有一个线程会执行 {} 的语句
+    	{
+      		for(int i = 0; i < 3; i++)
+      		{
+        		#pragma omp task  // 动态创建 tasks ，空闲线程会去执行 task
+     			func(a[i]);
+			}
+   		 }
+	}
+}
